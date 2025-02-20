d259d8ae3887ea4724e633b37cac9951c1f070d8
Mateolr28 <mateolrdev@outlook.com>
2025-02-19 21:50:18 -0500

    codigo encabezado

--- lecturacsv.py
+++ lecturacsv.py
@@ -1,6 +1,13 @@
+#Modulo nativo
 import csv 
 
 with open ("datos_prueba.csv", newline="", encoding="utf-8") as file:
     reader = csv.reader(file)
     for row in reader:
         print(row) 
+
+#Si el CSV tiene encabezados y quieres acceder a los datos como diccionario:
+with open("datos_prueba.csv", newline="", encoding="utf-8") as file:
+    reader = csv.DictReader(file)  # Lee como diccionarios
+    for row in reader:
+        print(row)  # Cada fila es un diccionario
